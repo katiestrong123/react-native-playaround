34cfd16417f4571c9b35847e8262e1b1652b5f86
Katie Strong <katiestrong123@gmail.com>
2021-03-23 23:17:56 +0000

    listing screen implemented and styled

--- App.js
+++ App.js
@@ -7,17 +7,13 @@ import AppText from "./app/components/AppText";
 // import Heading from "./app/components/Heading";
 import Card from "./app/components/Card";
 import AppButton from "./app/components/AppButton";
+import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
 
 export default function App() {
   return (
     // <WelcomeScreen />
     // <ItemScreen />
-    <Card
-      title={"Red Jacket for sale!"}
-      subTitle={"£100"}
-      userName={"Cassia Briscoe"}
-      numberOfListings={5}
-      itemImage={require("./app/assets/redJacket.jpeg")}
-    />
+    <ListingDetailsScreen />
+    
   );
 }
