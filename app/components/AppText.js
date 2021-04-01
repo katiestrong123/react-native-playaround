fc6157085db384326199645eedf20fb178ede36d
Katie Strong <katiestrong123@gmail.com>
2021-04-01 11:24:32 +0100

    extracting default styles

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,26 +1,10 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text } from "react-native";
 import colors from "../config/colors";
+import defaultStyles from "../config/styles";
 
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
-
-const styles = StyleSheet.create({
-  text: {
-    ...Platform.select({
-      ios: {
-        fontSize: 20,
-        fontWeight: 600,
-        fontFamily: "Avenir",
-      },
-      android: {
-        fontSize: 18,
-        fontWeight: 600,
-        fontFamily: "Roboto",
-      },
-    }),
-  },
-});
 
 export default AppText;
--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { View, StyleSheet, Platform } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { TextInput } from "react-native-gesture-handler";
 
-import colors from "../config/colors";
+import defaultStyles from "../config/styles";
 
 function AppTextInput({ icon, ...otherProps }) {
   return (
@@ -12,31 +12,26 @@ function AppTextInput({ icon, ...otherProps }) {
         <MaterialCommunityIcons
           name={icon}
           sie={20}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
           style={styles.icon}
         />
       )}
-      <TextInput style={styles.textInput} {...otherProps} />
+      <TextInput style={defaultStyles.text} {...otherProps} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
-    width: "100%",
-    padding: 15,
     marginVertical: 10,
+    padding: 15,
+    width: "100%",
   },
   icon: {
     margin: 10,
-  },
-  textInput: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    color: colors.dark,
   },
 });
 
